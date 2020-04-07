bea7533b4597bba1843debaa87d3a8c131c28981
gungamerr <nolosonie1@gmail.com>
2020-04-07 14:57:26 -0300

    springboot no anda

---
+++ src/main/java/com/utn/utn/UtnApplication.java
@@ -0,0 +1,13 @@
+package com.utn.utn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/utn/UtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.utn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
